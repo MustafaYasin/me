8035f1b9a3b87968e4312508ade53e087b5cb195
Mustafa Yasin <mustafa.yasin2404@gmail.com>
2020-04-22 00:22:33 +0200

    Updates

---
+++ precache-manifest.5d048a402416b61075db1b0101709fd1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaeb7ce10f7b4591360a45f190b6b3a1",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "8b217b5cd58c38092704",
+    "url": "/homepage/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "5672ff0a7c61fc29b651",
+    "url": "/homepage/static/css/main.fb69c0e6.chunk.css"
+  },
+  {
+    "revision": "8b217b5cd58c38092704",
+    "url": "/homepage/static/js/2.1c13e8ee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/homepage/static/js/2.1c13e8ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5672ff0a7c61fc29b651",
+    "url": "/homepage/static/js/main.f32eeccc.chunk.js"
+  },
+  {
+    "revision": "3240d77118c7bfefaf4d",
+    "url": "/homepage/static/js/runtime-main.e90a7725.js"
+  },
+  {
+    "revision": "b980a14d497a9561bcbda63259d91a11",
+    "url": "/homepage/static/media/chronology_logo.b980a14d.svg"
+  },
+  {
+    "revision": "520856de922c847f2ec684c079678491",
+    "url": "/homepage/static/media/profile.520856de.png"
+  }
+]);
