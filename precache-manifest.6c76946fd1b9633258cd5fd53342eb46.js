1cf2d4c757f3c269a276b212d126ddebbef3e3d7
Mustafa Yasin <myasin@adobe.com>
2022-08-29 20:18:14 +0200

    Updates

---
+++ precache-manifest.6c76946fd1b9633258cd5fd53342eb46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a8ef3ca950ff968a690dd700e87d830",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07ded543c893281f4436",
+    "url": "/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "794cc8b1a5beffe6db30",
+    "url": "/static/css/main.c8e1ec73.chunk.css"
+  },
+  {
+    "revision": "07ded543c893281f4436",
+    "url": "/static/js/2.45200c7a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.45200c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "794cc8b1a5beffe6db30",
+    "url": "/static/js/main.b123df5d.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "ebffb56929f87acd2892abae91bda51d",
+    "url": "/static/media/profile.ebffb569.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homepage/precache-manifest.ee13d1e1dcd65a14849dd7525febe6fc.js"
+  "/precache-manifest.6c76946fd1b9633258cd5fd53342eb46.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homepage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
